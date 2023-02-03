64719b4efe32efb1eb38f586c4d855a70185e707
jaeyoung <kjy0302014@gmail.com>
2023-02-03 20:38:31 +0900

    BaseBallResult getBaseBallResult,validationPlayerSelect, getSameNumberCnt, getMatchNumberCnt 메서드 추가 및 테스트 코드 작성

---
+++ src/main/kotlin/baseball/view/BaseBallView.kt
@@ -0,0 +1,37 @@
+package baseball.view
+
+import baseball.model.BaseBallResult
+import camp.nextstep.edu.missionutils.Console
+import java.lang.StringBuilder
+
+class BaseBallView {
+    fun startGame(){
+        println("숫자 야구 게임을 시작합니다.")
+    }
+
+    fun inputNumber() : String{
+        print("숫자를 입력해주세요 : ")
+        val num = Console.readLine()
+        println()
+        return num
+    }
+
+    fun printResult(result : BaseBallResult){
+        StringBuilder().apply {
+            if(result.hasBall())
+                this.append("${result.ballCnt}볼 ")
+            if(result.hasStrike())
+                this.append("${result.strikeCnt}스트라이크")
+            if(result.isNothing())
+                this.append("낫싱")
+        }.also {
+            println(it)
+        }
+    }
+
+    fun finishGame() : Int{
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        return Console.readLine().toInt()
+    }
+}
--- src/test/kotlin/baseball/BaseBallResultTest.kt
+++ src/test/kotlin/baseball/BaseBallResultTest.kt
@@ -2,7 +2,9 @@ package baseball
 
 import baseball.model.BaseBallResult
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
 
 class BaseBallResultTest {
 
@@ -28,5 +30,32 @@ class BaseBallResultTest {
     fun baseBallResultIsNothingTest(){
         val baseBallResult = BaseBallResult(0,0)
         Assertions.assertThat(baseBallResult.isNothing()).isTrue
+    }
+
+    @Test
+    fun getBaseBallResultTest(){
+        val target = listOf(1,3,4)
+        val player = "143"
+        val result = BaseBallResult.getBaseBallResult(target,player)
+        val baseBallResult = BaseBallResult(1,2)
+        Assertions.assertThat(result).isEqualTo(baseBallResult)
+    }
+
+    @Test
+    fun getBaseBallResultExceptionOverLengthTest(){
+        val target = listOf(1,3,4)
+        val player = "1434"
+        assertThrows(IllegalArgumentException::class.java){
+            BaseBallResult.getBaseBallResult(target,player)
+        }
+    }
+
+    @Test
+    fun getBaseBallResultExceptionInvalidTypeTest(){
+        val target = listOf(1,3,4)
+        val player = "aaaaa"
+        assertThrows(IllegalArgumentException::class.java){
+            BaseBallResult.getBaseBallResult(target,player)
+        }
     }
 }
